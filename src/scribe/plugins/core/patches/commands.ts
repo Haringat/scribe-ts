7c15ffb187e088b8e66ee9640203bca950db327f
Rasmus Schultz <rasc@fynskemedier.dk>
2016-06-03 10:19:38 +0200

    fix includes

--- src/scribe/plugins/core/patches/commands.ts
+++ src/scribe/plugins/core/patches/commands.ts
@@ -1,9 +1,9 @@
-import bold = require("./patches/commands/bold")
-import indent = require("./patches/commands/indent")
-import insertHTML = require("./patches/commands/insert-html")
-import insertList = require("./patches/commands/insert-list")
-import outdent = require("./patches/commands/outdent")
-import createLink = require("./patches/commands/create-link")
+import bold = require("./commands/bold")
+import indent = require("./commands/indent")
+import insertHTML = require("./commands/insert-html")
+import insertList = require("./commands/insert-list")
+import outdent = require("./commands/outdent")
+import createLink = require("./commands/create-link")
 
 export {
     bold,
